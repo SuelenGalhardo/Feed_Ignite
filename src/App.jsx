f82166c62fdf5a03a1df569ff9f97d0f52f62f64
SuelenGalhardo <suelenranucci@gmail.com>
2024-07-17 15:59:53 +0200

    added component Post

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,11 @@ export function App() {
         content= "lorem ipsum dolor sit amet, consectetur adip"
         
         />
+            <Post
+        author="Otto Ranucci "
+        content= "lorem ipsum dolor sit amet, consectetur adip"
+        
+        />
       </main>
 
      </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,35 @@
 
-  export function Post(props) {
+  export function Post() {
     return (
-      <div>
-        <strong>{props.author}</strong>
-        <p>{props.content}</p>
-      </div>
+      <>
+
+      <article className="post">
+        <header>
+        <div className="post__author">
+          <img className="post__avatar" src="https://github.com/diego3g.png" />
+
+          <div className="post__authorInfo">
+            <strong className="post__authorInfoName">Diego Fernandes</strong>
+            <span className="post__authorInfoCareer">web developer</span>
+
+          </div>
+        </div>
+
+      <time className="post__time" dateTime=" 2022-07-11  08:13:30 ">Publicado hace una 1h</time>
+
+
+        </header>
+
+        <div className="post__content">
+          <p className="post__content--text">Hola Chic@s </p>
+          <p  className="post__content--text">Acabo de subir un poyecto en mi portafolio. Es uno que hizo en el NLWJorney, con NODE.js </p>
+
+          <p><a className="post__content--link" href="">jane.design/doctorcare</a></p>
+
+          <p><a className="post__content--link" href="">#nuevoproyecto #Nlw #rocketseat</a></p>
+        </div>
+      </article>
+       
+      </>
     )
   }
