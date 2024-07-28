fe9473a5465aaaf612fd5a0105ea798c1255d06f
SuelenGalhardo <suelenranucci@gmail.com>
2024-07-28 21:47:47 +0200

    added post html and css

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,6 @@
 
+
+
   export function Post() {
     return (
       <>
@@ -9,10 +11,11 @@
           <img className="post__avatar" src="https://github.com/diego3g.png" />
 
           <div className="post__authorInfo">
-            <strong className="post__authorInfoName">Diego Fernandes</strong>
-            <span className="post__authorInfoCareer">web developer</span>
+            <strong className="post__authorInfo--name">Diego Fernandes</strong>
+            <span className="post__authorInfo--career">web developer</span>
 
           </div>
+          
         </div>
 
       <time className="post__time" dateTime=" 2022-07-11  08:13:30 ">Publicado hace una 1h</time>
@@ -21,13 +24,28 @@
         </header>
 
         <div className="post__content">
-          <p className="post__content--text">Hola Chic@s </p>
+          <p className="post__content--text">Hola Chic@s 👋 </p>
           <p  className="post__content--text">Acabo de subir un poyecto en mi portafolio. Es uno que hizo en el NLWJorney, con NODE.js </p>
 
-          <p><a className="post__content--link" href="">jane.design/doctorcare</a></p>
+          <p><a className="post__content--link" href=""> 👉 jane.design/doctorcare</a></p>
 
-          <p><a className="post__content--link" href="">#nuevoproyecto #Nlw #rocketseat</a></p>
+          <p><a className="post__content--link" href="">#nuevoproyecto</a>{' '}
+          <a className="post__content--link"  href="">#nlw</a>{' '}
+          <a className="post__content--link"  href="">#rocketseat</a>
+          </p>
         </div>
+        <form className="post__form">
+        <strong className="port__form--feedback">Deja tu feedback</strong>
+
+        <textarea className="post__form--commit" 
+          placeholder="Deja tu comentario"
+        />
+
+        <footer className="footerBtn">
+          <button className="footer__button" type="submit">Publicar</button>
+        </footer>
+      </form>
+
       </article>
        
       </>
