078b55c293fe0fc0836b8cc3c3a90c364085b74d
SuelenGalhardo <suelenranucci@gmail.com>
2024-07-30 14:40:02 +0200

    added const posts

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,38 @@ import { Post } from "./components/Post"
 import { Sidebar } from "./components/Sidebar"
 import "./styles/layout/Main.scss"
 
+const posts = [
+{
+id: 1, 
+author: {
+  avatarUrl: 'http://fithug.com/SuelenGalhardo.png',
+  name: 'Suelen Ranucci',
+  career: 'web developer',
+  role: 'web developer',
+},
+
+content: [ { type: 'paragraph', content: 'Fala galera 👋' },
+  { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+  { type: 'link', content: 'jane.design/doctorcare' },
+],
+publishedAt: new Date('2022-05-03 20:00:00'),
+},
+{
+id: 2,
+author: {
+  avatarUrl: 'https://github.com/maykbrito.png',
+  name: 'Mayk Brito',
+  role: 'Educator @Rocketseat'
+},
+content: [
+  { type: 'paragraph', content: 'Fala galera 👋' },
+  { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+  { type: 'link', content: 'jane.design/doctorcare' },
+],
+publishedAt: new Date('2022-05-10 20:00:00'),
+},
+
+]
 
 
 export function App() {
