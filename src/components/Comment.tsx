fe82f4e99b8997c9d775caa3b4661d63dec0d110
SuelenGalhardo <suelenranucci@gmail.com>
2024-07-30 12:45:39 +0200

    added css comment

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -6,27 +6,28 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 export function Comment() {
   return (
     <div className='comment'>
-      <img src="https://github.com/SuelenGalhardo.png" alt="" />
+      <img className='comment__img' src="https://github.com/SuelenGalhardo.png" alt="" />
 
       <div className="comment__commentBox">
         <div className="comment__Content">
             
-          <header>
-            <div className= "comment__authorAndTime">
+          <header className='comment__header'>
+            <div className= "comment__header--authorAndTime">
               <strong>Suelen Ranucci</strong>
-              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">hace 1h</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button className='comment__header--buttonDelete' title="Deletar comentário">
               <Trash size={20} />
+              
             </button>
           </header>
 
-          <p>Muito bom Devon, parabéns!! 👏👏</p>
+          <p>Muy bien, enhorabuena!! 👏👏</p>
         </div>
 
-        <footer>
-          <button>
+        <footer className='comment__footer'>
+          <button className='comment__btnApplaud'>
             <ThumbsUp />
             Aplaudir <span>20</span>
           </button>
